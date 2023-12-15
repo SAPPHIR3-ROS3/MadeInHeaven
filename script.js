e6ef3fcdfe86599c9587ac8345a7055ef6e1f689
SAPPHIR3-ROS3 <patriziosimone@hotmail.it>
2023-12-15 11:39:22 +0100

    general code optimization + MadeInheaven phase 4 partial realization

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const TheWorldStart = new Audio('./media/audio/Star Platinum The World Start.mp3');
+const TheWorldEnd = new Audio('./media/audio/Star Platinum The World End.mp3');
+const Crucified = new Audio('./media/audio/Crucified Army of Lovers.mp3');
+const MadeInHeavenAudio = new Audio('./media/audio/MadeInHeaven.mp3');
 let OrbitPeriodMultiplier = [1/3, 2/3, 3/3 , 4/3, 6/3, 7/3, 8/3, 9/3, 10/3];
 let SatelliteOrbits = 
 {
@@ -20,29 +24,29 @@ let EarthYear = 60;
 let PreviousTimeSpeed = 0;
 let TimeSpeed = 1;
 let RandomPosition = true;
-let MadeInHeavenPhase1 = false;
+let MadeInHeavenIsRunning = false;
 let MadeInHeavenPhase2 = false;
 let MadeInHeavenPhase3 = false;
 let ZaWarudoIsRunning = false;
 
 function ToggleBWFilter(seconds, SecondsDelay = 0) {
-    function BWPercentage(gray) {document.body.style.filter = `grayscale(${gray}%)`;}
-  
-    if(TimeSpeed == 0)
-    {
-        for(let gray = 0; gray < 101; gray++)
-            setTimeout(() => {BWPercentage(gray);}, SecondsDelay * 1000 + gray * (seconds - SecondsDelay) * 10);
+    let GrayDelta = TimeSpeed === 0 ? 1 : -1;
+    let InitialGray = TimeSpeed === 0 ? 0 : 100;
+    let duration = (seconds - SecondsDelay) * 10;
+
+    function BWPercentage(gray) {
+        document.body.style.filter = `grayscale(${gray}%)`;
     }
-    else
-    {
-        for(let gray = 0; gray < 101; gray++)
-            setTimeout(() => {BWPercentage(100 - gray);}, SecondsDelay * 1000 + gray * (seconds - SecondsDelay) * 10);
-    }
+
+    for(let gray = 0; gray < 101; gray++) 
+        setTimeout(BWPercentage.bind(null, InitialGray + GrayDelta * gray), SecondsDelay * 1000 + gray * duration);
 }
 
 function ManageRing(seconds, SecondsDelay) {
     const ring = document.createElement('div');
     let Earth = document.querySelector('.earth');
+    let DiameterDelta = TimeSpeed === 0 ? 1 : -1;
+    let InitialDiameter = TimeSpeed === 0 ? 0 : 100;
     ring.classList.add('ring');
     Earth.appendChild(ring);
     
@@ -55,23 +59,14 @@ function ManageRing(seconds, SecondsDelay) {
         ring.style.height = diameter + 'px';
     }
   
-    if(TimeSpeed == 0)
-    {
-      for(let diameter = 0; diameter < 101; diameter++)
-        setTimeout(() => {change(diameter);}, SecondsDelay * 1000 + diameter * (seconds - SecondsDelay) * 10);
+    for(let diameter = 0; diameter < 101; diameter++)
+        setTimeout(change.bind(null, InitialDiameter + DiameterDelta * diameter), SecondsDelay * 1000 + diameter * (seconds - SecondsDelay) * 10);
   
-      setTimeout(() => {ring.remove();}, seconds * 1000);
-    }
-    else
-    {
-      for(let diameter = 0; diameter < 101; diameter++)
-        setTimeout(() => {change(100 - diameter);}, diameter * seconds * 10);
-  
-      setTimeout(() => {ring.remove();}, seconds * 1000);
-    }
+    setTimeout(ring.remove.bind(ring), seconds * 1000);
 }
 
 function Clock(seconds) {
+    let opacity = 0;
     let FadeTime = seconds / 12;
     let exponent = 4.1;
     let StartTime = performance.now();
@@ -81,12 +76,13 @@ function Clock(seconds) {
     let Watch = document.createElement('div');
     Watch.classList.add('watch');
     let WatchFace = document.createElement('div');
-    WatchFace.classList.add('watchface');
     let HoursHand = document.createElement('div');
-    HoursHand.classList.add('hours-hand');
     let MinutesHand = document.createElement('div');
-    MinutesHand.classList.add('minutes-hand');
     const AngleIncrement = (2 * Math.PI) / (60 * EarthYear);
+    const increment = AngleIncrement * TimeSpeed * (Math.log(TimeSpeed))**exponent;
+    WatchFace.classList.add('watchface');
+    HoursHand.classList.add('hours-hand');
+    MinutesHand.classList.add('minutes-hand');
     Watch.appendChild(WatchFace);
     Watch.appendChild(MinutesHand);
     Watch.appendChild(HoursHand);
@@ -100,30 +96,45 @@ function Clock(seconds) {
         MinutesHand.style.opacity = opacity;
     };
 
+    function fadeIn() {
+        if (opacity < 51) {
+            ClockOpacity(opacity/100);
+            opacity++;
+            requestAnimationFrame(fadeIn);
+        }
+    }
+
+    function fadeOut() {
+        if (opacity > -1) {
+            ClockOpacity(opacity/100);
+            opacity--;
+            requestAnimationFrame(fadeOut);
+        } 
+        else {
+            Watch.remove();
+        }
+    }
+
     function updateClockPosition() {
         if (TimeSpeed !== 0) {
-            HoursDeg = HoursDeg + (AngleIncrement * TimeSpeed * 1 / 12 * (Math.log(TimeSpeed))**exponent);
-            MinutesDeg = MinutesDeg + (AngleIncrement * TimeSpeed * (Math.log(TimeSpeed))**exponent);
+            HoursDeg += increment / 12;
+            MinutesDeg += increment;
             HoursHand.style.transform = `translate(-50%, -50%) rotate(${HoursDeg}deg)`;
             MinutesHand.style.transform = `translate(-50%, -50%) rotate(${MinutesDeg}deg)`;
         }
     }
 
     function ClockFrame() {
-        if((performance.now() - StartTime) / 1000 < seconds)
+        if((performance.now() - StartTime) / 1000 < seconds - FadeTime/10)
         {
             updateClockPosition();
             requestAnimationFrame(ClockFrame);
         }
         else
-        {
-            document.body.removeChild(Watch);
-        }
+            fadeOut();
     }
 
-    for(let opacity = 0; opacity < 51; opacity++)
-        setTimeout(() => {ClockOpacity(opacity/100);}, opacity * FadeTime * 10);
-
+    fadeIn();
     ClockFrame();
 }
 
@@ -131,8 +142,8 @@ function DaylightCycle(seconds) {
     let exponent = 2;
     let StartTime = performance.now();
     let btw = document.createElement('div');
-    btw.classList.add('night-to-day');
     let wtb = document.createElement('div');
+    btw.classList.add('night-to-day');
     wtb.classList.add('day-to-night');
     document.body.appendChild(btw);
     document.body.appendChild(wtb);
@@ -168,7 +179,7 @@ function DaylightCycle(seconds) {
 }
   
 function ZaWarudo() {
-    if(!ZaWarudoIsRunning)
+    if(!ZaWarudoIsRunning || MadeInHeavenIsRunning)
     {
         ZaWarudoIsRunning = true;
         setTimeout(() => 
@@ -179,8 +190,7 @@ function ZaWarudo() {
         { 
             PreviousTimeSpeed = TimeSpeed;
             TimeSpeed = 0;
-            let TheWorld = new Audio('./media/audio/Star Platinum The World Start.mp3')
-            TheWorld.play();
+            TheWorldStart.play();
             ToggleBWFilter(4, 2);
             ManageRing(5, 2);
         }
@@ -188,8 +198,7 @@ function ZaWarudo() {
         {
             TimeSpeed = PreviousTimeSpeed;
             PreviousTimeSpeed = 0;
-            let TheWorld = new Audio('media/audio/Star Platinum The World End.mp3')
-            TheWorld.play();
+            TheWorldEnd.play();
             ToggleBWFilter(3);
             ManageRing(2);
         }
@@ -198,53 +207,122 @@ function ZaWarudo() {
 
 function MadeInHeaven() {
 
-     if(!MadeInHeavenPhase1)
+     if(!MadeInHeavenIsRunning)
      {
-        MadeInHeavenPhase1 = true;
+        MadeInHeavenIsRunning = true;
         let SpeedPhase1 = 2901;
         let SpeedPhase2 = 6501;
-        let SpeedPhase3 = 2**19;
+        let SpeedPhase3 = 10001;
+
+        let DPhase0 = 17;
+        let DPhase1 = 5;
+        let DPhase2 = 14;
+        let DPhase3 = 31;
+        let DPhase4 = 38;
 
         let MadeInHeavenText = document.createElement('div');
         MadeInHeavenText.id = 'MadeInHeaven';
         MadeInHeavenText.innerHTML = 'Made in<br>Heaven';
-
-        let Crucified = new Audio('./media/audio/Crucified Army of Lovers.mp3');
+        let StartTime = performance.now();
+        let CurrentTime;
+        
         Crucified.play();
 
         setTimeout(() =>
         {
+            console.log('start');
+
             for(let i = 0; i < SpeedPhase1; i++)
                 setTimeout(() => {TimeSpeed = 1+i/100;}, i/SpeedPhase1 * SpeedPhase1);
             
-        }, 17 * 1000);
+            CurrentTime = performance.now();
+            console.log((CurrentTime - StartTime) / 1000);
+        }, DPhase0 * 1000);
         setTimeout(() =>
         {
-            let MadeInHeavenAudio = new Audio('./media/audio/MadeInHeaven.mp3');
             MadeInHeavenAudio.play();
             document.body.appendChild(MadeInHeavenText);
-        }, 16 * 1000);
+        }, (DPhase0 - 1) * 1000);
         setTimeout(() =>
         {
+            console.log('Phase 1');
+            Clock(DPhase2);
+
             for(let i = SpeedPhase1 + 99; i < SpeedPhase2; i++)
-                setTimeout(() => {TimeSpeed = i/100;}, (i - SpeedPhase1 - 99)/(SpeedPhase2 - SpeedPhase1) * 49 * 1000);
+                setTimeout(() => {TimeSpeed = i/100;}, (i - SpeedPhase1 - 99)/(SpeedPhase2 - SpeedPhase1) * DPhase2 * 1000);
 
-            Clock(49);
             document.body.removeChild(MadeInHeavenText);
-        }, 21 * 1000);
+            CurrentTime = performance.now();
+            console.log((CurrentTime - StartTime) / 1000);
+        }, (DPhase0 + DPhase1) * 1000);
         setTimeout(() =>
         {
+            console.log('Phase 2');
+            DaylightCycle(DPhase3);
+
             for(let i = SpeedPhase2 - 1; i < SpeedPhase3; i++)
-                setTimeout(() => {TimeSpeed = i/100;}, (i - SpeedPhase2 + 1)/(SpeedPhase3 - SpeedPhase2) * 56 * 1000);
-
-            DaylightCycle(56);
-            MadeInHeavenPhase1 = false;
-        }, 70 * 1000);
+            {
+                if(i % 200 == 0)
+                    setTimeout(() => {TimeSpeed = i/100;}, (i - SpeedPhase2 + 1)/(SpeedPhase3 - SpeedPhase2) * DPhase3 * 1000);
+            }
+            CurrentTime = performance.now();
+            console.log((CurrentTime - StartTime) / 1000);
+        }, (DPhase0+DPhase1+DPhase2) * 1000);
         setTimeout(() =>
         {
+            console.log('Phase 3');
 
-        },126.5 * 1000)
-        setTimeout(() => {Crucified.pause();}, 126.5 * 1000);
+            TimeSpeed = 0;
+            let orbits = document.querySelector('.orbits');
+            let planets = Array.from(document.querySelectorAll('.solar-object')).slice(1);
+            let asteroids = Array.from(document.querySelectorAll('.asteroids-belt-rock'));
+            let stars = Array.from(document.querySelectorAll('.star-dot'));
+            let VacuumTime = DPhase4 - 2;
+
+            orbits.remove();
+
+            function VacuumUniverse() {
+                for(let i = 0; i < asteroids.length; i++)
+                {
+                    let angle = Math.atan2(window.innerHeight / 2 - asteroids[i].offsetTop, window.innerWidth / 2 - asteroids[i].offsetLeft) * 180 / Math.PI;
+                    SpiralAround(asteroids[i], 3.5, VacuumTime + ((Math.random() * 4) - 2), angle);
+                }
+
+                for(let i = 0; i < stars.length; i++)
+                {
+                    let angle = Math.atan2(window.innerHeight / 2 - stars[i].offsetTop, window.innerWidth / 2 - stars[i].offsetLeft) * 180 / Math.PI;
+                    SpiralAround(stars[i], 10.5, VacuumTime + ((Math.random() * 4) - 2), angle);
+                }
+
+                for(let i = 0; i < planets.length; i++)
+                {
+                    let satellites = Array.from(planets[i].children);
+                    let SpiralTime = VacuumTime + ((Math.random() * 4) - 2);
+                    SpiralAround(planets[i], i + 0.5, SpiralTime,Number(planets[i].dataset.angle));
+
+                    for(let j = 0; j < satellites.length; j++)
+                    {
+                        let SatelliteSpiralTime = SpiralTime + j/10;
+                        let angle = Math.atan2(window.innerHeight / 2 - satellites[j].offsetTop, window.innerWidth / 2 - satellites[j].offsetLeft) * 180 / Math.PI;
+                        SpiralAround(satellites[j], i + 0.5, SatelliteSpiralTime, angle);
+                    }
+                }
+
+                requestAnimationFrame(VacuumUniverse);
+            }
+
+            VacuumUniverse();
+
+            CurrentTime = performance.now();
+            console.log((CurrentTime - StartTime) / 1000);
+        }, (DPhase0+DPhase1+DPhase2+DPhase3) * 1000);
+        setTimeout(() => 
+        {
+            console.log('end');
+            TimeSpeed = 0;
+            MadeInHeavenIsRunning = false;
+            Crucified.pause();
+        }, (DPhase0+DPhase1+DPhase2+DPhase3+3) * 1000);
      }
 }
 
@@ -378,12 +456,11 @@ function AsteroidsBeltSpin()
     let Sun = document.querySelector('.center');
     let asteroids = Array.from(document.querySelectorAll('.asteroids-belt-rock'));
     let OrbitTime = 5/3 * EarthYear;
-    //console.log(OrbitTime);
 
     for(let i = 0; i < asteroids.length; i++)
     {
         //console.log(Sun, asteroids[i], Number(asteroids[i].dataset.radius), Number(asteroids[i].dataset.angle), OrbitTime);
-        //OrbitAround(Sun, asteroids[i], Number(asteroids[i].dataset.radius), Number(asteroids[i].dataset.angle), OrbitTime);
+        OrbitAround(Sun, asteroids[i], Number(asteroids[i].dataset.radius), Number(asteroids[i].dataset.angle), OrbitTime);
     }
 }
 
@@ -392,7 +469,7 @@ function OrbitAround(center, OrbitingObject, radius, angle = 0, OrbitTime = 1) {
     const AngleIncrement = (2 * Math.PI) / (60 * OrbitTime);
   
     function updatePosition() {
-        if (TimeSpeed !== 0)
+        if (TimeSpeed !== 0 || !MadeInHeavenIsRunning)
         {
             radian += AngleIncrement * TimeSpeed;
     
@@ -417,7 +494,7 @@ function OrbitAround(center, OrbitingObject, radius, angle = 0, OrbitTime = 1) {
 }
 
 function SpiralAround(object, loops, duration, initialAngle) {
-    let startTime = null;
+    let startTime = performance.now();
     let centerX = window.innerWidth / 2;
     let centerY = window.innerHeight / 2;
     let objectCenterX = object.offsetLeft + object.offsetWidth / 2;
@@ -427,7 +504,6 @@ function SpiralAround(object, loops, duration, initialAngle) {
 
     function spiralMovement() {
         let currentTime = performance.now();
-        if (!startTime) startTime = currentTime;
         let elapsed = (currentTime - startTime) / 1000; // convert to seconds
         let angle = initialAngle + Math.min(1, (elapsed / duration)) * 2 * Math.PI * loops;
         let radius = initialRadius * (1 - Math.min(1, elapsed / duration));
@@ -504,6 +580,9 @@ function()
 window.addEventListener('resize', 
 function()
 {
-    RepositionOrbits();
-    RepositionAsteroids();
+    if(!MadeInHeavenIsRunning)
+    {
+        RepositionOrbits();
+        RepositionAsteroids();
+    }
 });
